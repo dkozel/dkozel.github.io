cb04121f4679b9817a0f6aa1fafb6ae6abe1147e
Derek Kozel <derek@bitstovolts.com>
2019-12-28 17:38:45 +0000

    circleci: Added initial build config

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+set -e # stop at first failing command
+
+hugo -v
+cd public
+
+git init
+git config user.name "Derek Kozel"
+git config user.email "derek@bitstovolts.com"
+
+echo "www.derekkozel.com" > CNAME
+git add .
+git commit -m "Deploy from CircleCI"
+
+# use --quiet to avoid printing token in build logs
+git push --force --quiet "https://${GH_TOKEN}@github.com/dkozel/dkozel.github.io.git" master:gh-pages
